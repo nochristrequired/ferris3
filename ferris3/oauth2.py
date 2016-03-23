d3f42e0bb691f3497c8dd0df08d91d9e70b382e0
Paul Woelfel <paul.woelfel@zirrus.eu>
2016-03-23 15:26:40 +0100

    SignedJWTAssertionCredential has been removed and replaced by ServiceAccountCredentials. Changes will make ferris3 work with oauth2client version 1.5 and 2.0

--- ferris3/oauth2.py
+++ ferris3/oauth2.py
@@ -4,6 +4,7 @@ import hashlib
 import endpoints
 import os
 from oauth2client.client import AccessTokenCredentials
+from oauth2client.service_account import ServiceAccountCredentials
 
 
 def get_endpoints_credentials():
@@ -48,22 +49,28 @@ def build_service_account_credentials(scope, user=None):
     Builds service account credentials using the configuration stored in :mod:`~ferris3.settings`
     and masquerading as the provided user.
     """
-    if not SignedJwtAssertionCredentials:
-        raise EnvironmentError("Service account can not be used because PyCrypto is not available. Please install PyCrypto.")
-
     config = _get_config()
 
-    if not isinstance(scope, (list, tuple)):
-        scope = [scope]
+    if hasattr(ServiceAccountCredentials, "from_json_keyfile_dict"):
+        # running oauth2client version 2.0
+        creds = ServiceAccountCredentials.from_json_keyfile_dict(config, scope)
+        if user is not None:
+            creds.create_delegated(user)
+    else:
+        if not SignedJwtAssertionCredentials:
+            raise EnvironmentError("Service account can not be used because PyCrypto is not available. Please install PyCrypto.")
 
-    key = _generate_storage_key(config['client_email'], scope, user)
-    storage = StorageByKeyName(ServiceAccountStorage, key, 'credentials')
+        if not isinstance(scope, (list, tuple)):
+            scope = [scope]
 
-    creds = SignedJwtAssertionCredentials(
-        service_account_name=config['client_email'],
-        private_key=config['private_key'],
-        scope=scope,
-        prn=user)
+        key = _generate_storage_key(config['client_email'], scope, user)
+        storage = StorageByKeyName(ServiceAccountStorage, key, 'credentials')
+
+        creds = SignedJwtAssertionCredentials(
+            service_account_name=config['client_email'],
+            private_key=config['private_key'],
+            scope=scope,
+            prn=user)
     creds.set_store(storage)
 
     return creds
